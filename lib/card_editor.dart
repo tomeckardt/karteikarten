bdd450f2c131cba11e982566f9d2e3e7b51ab3a9
Tom Eckardt <Ta10top-twcubatwc>
2021-12-20 23:49:34 +0100

    Code schöner

--- lib/card_editor.dart
+++ lib/card_editor.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
 import 'package:mcapp/edit_deck.dart';
 
@@ -20,80 +19,84 @@ class _CardEditorState extends State<CardEditor> {
   String _newDeckName = '';
 
   @override
-  void initState() {
-    _decks = Hive.box("decks");
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Decks")),
-      body: _decks.isEmpty
-        ? const Center(child: Text("Es gibt keine Decks."))
-        : ListView.builder(
-          itemCount: _decks.length,
-          itemBuilder: (context, index) {
-            var correspondingDeck = _decks.getAt(index);
-            return Card(
-                child: ListTile(
-                  title: Text(correspondingDeck.name),
-                  leading: IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () => setState(() {
-                      _decks.deleteAt(index);
-                    })
-                  ),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.arrow_forward),
-                    onPressed: () {
-                      final page = EditDeck(deck: correspondingDeck);
-                      Navigator.of(context).push(
-                          PageRouteBuilder(
-                            pageBuilder: (context, animation, an2) => page,
-                            transitionDuration: const Duration(milliseconds: 100),
-                            reverseTransitionDuration: const Duration(milliseconds: 100),
-                            transitionsBuilder: (context, an1, an2, child) {
-                              return SlideTransition(
-                                position: Tween(
-                                    begin: const Offset(1.0, 0.0),
-                                    end: const Offset(0.0, 0.0))
-                                    .animate(an1),
-                                child: child,
-                              );
-                            }
+      bottomSheet: FutureBuilder(
+        future: Hive.openBox("decks").then((value) => _decks = Hive.box("decks")),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Scaffold(
+                body: _decks.isEmpty
+                    ? const Center(child: Text("Es gibt keine Decks."))
+                    : ListView.builder(
+                    itemCount: _decks.length,
+                    itemBuilder: (context, index) {
+                      var correspondingDeck = _decks.getAt(index);
+                      return Card(
+                          child: ListTile(
+                            title: Text(correspondingDeck.name),
+                            leading: IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () => setState(() {
+                                  _decks.deleteAt(index);
+                                })
+                            ),
+                            trailing: IconButton(
+                              icon: const Icon(Icons.arrow_forward),
+                              onPressed: () {
+                                final page = EditDeck(deck: correspondingDeck);
+                                Navigator.of(context).push(
+                                    PageRouteBuilder(
+                                        pageBuilder: (context, animation, an2) => page,
+                                        transitionDuration: const Duration(milliseconds: 100),
+                                        reverseTransitionDuration: const Duration(milliseconds: 100),
+                                        transitionsBuilder: (context, an1, an2, child) {
+                                          return SlideTransition(
+                                            position: Tween(
+                                                begin: const Offset(1.0, 0.0),
+                                                end: const Offset(0.0, 0.0))
+                                                .animate(an1),
+                                            child: child,
+                                          );
+                                        }
+                                    )
+                                );
+                              },
+                            ),
                           )
                       );
+                    }
+                ),
+                bottomNavigationBar: BottomAppBar(child: Padding(padding: const EdgeInsets.all(12), child: Row(
+                  children: [
+                    Expanded(child: TextField(
+                        decoration: InputDecoration(
+                            labelText: "Name des Decks",
+                            errorText: _decks.containsKey(_newDeckName)
+                                ? "Name bereits vergeben"
+                                : null
+                        ),
+                        onChanged: (value) => setState(() {
+                          _newDeckName = value;
+                        })
+                    )),
+                    const Padding(padding: EdgeInsets.all(12)),
+                    ElevatedButton.icon(onPressed: (_newDeckName == '' || _decks.containsKey(_newDeckName)) ? null :
+                        () {
+                      setState(() {
+                        _decks.put(_newDeckName, Deck(_newDeckName));
+                      });
                     },
-                  ),
-                )
-            );
+                        icon: const Icon(Icons.add), label: const Text("Deck")
+                    )
+                  ],
+                ),
+                )));
           }
-      ),
-      bottomNavigationBar: BottomAppBar(child: Padding(padding: const EdgeInsets.all(12), child: Row(
-        children: [
-          Expanded(child: TextField(
-            decoration: InputDecoration(
-              labelText: "Name des Decks",
-              errorText: _decks.containsKey(_newDeckName)
-                  ? "Name bereits vergeben"
-                  : null
-            ),
-            onChanged: (value) => setState(() {
-              _newDeckName = value;
-            })
-          )),
-          const Padding(padding: EdgeInsets.all(12)),
-          ElevatedButton.icon(onPressed: (_newDeckName == '' || _decks.containsKey(_newDeckName)) ? null :
-              () {
-                  setState(() {
-                    _decks.put(_newDeckName, Deck(_newDeckName));
-                  });
-              },
-              icon: const Icon(Icons.add), label: const Text("Deck")
-          )
-        ],
-      ),
-    )));
+          return const Center(child: Text("Decks werden geladen", style: TextStyle(color: Colors.grey)));
+        }
+      ));
+
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
@@ -8,11 +7,7 @@ import 'package:mcapp/card_editor.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:mcapp/card_deck.dart';
 
-import 'home.dart';
 
-var _pages = [const Home(), const CardEditor()];
-
-int _pageIndex = 0;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,51 +15,5 @@ void main() async {
   Hive.init(directory.path);
   Hive.registerAdapter(IndexCardAdapter());
   Hive.registerAdapter(DeckAdapter());
-  await Hive.openBox("decks");
-  await Hive.openBox("metadata");
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MainFrame()
-    );
-  }
-}
-
-class MainFrame extends StatefulWidget {
-  const MainFrame({Key? key}) : super(key: key);
-
-  @override
-  _MainFrameState createState() => _MainFrameState();
-}
-
-class _MainFrameState extends State<MainFrame> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_pageIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: "Karten")
-        ],
-        currentIndex: _pageIndex,
-        onTap: (int index) {
-          setState(() {
-            _pageIndex = index;
-          });
-        },
-      ),
-    );
-  }
+  runApp(const MaterialApp(home: CardEditor()));
 }
